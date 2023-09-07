47f50308e63b2f16b5dd35088f08ff0ef937096f
FournyP <pierre.fourny76@gmail.com>
2023-09-07 20:05:27 +0200

    Implement Dockerfile to run on Railway.app

---
+++ config.sh
@@ -0,0 +1,9 @@
+echo "
+{
+  \"storage\": { \"file\": { \"path\": \"${STORAGE_PATH}\" } },
+  \"listener\": [{ \"tcp\": { \"address\": \"0.0.0.0:8200\", \"tls_disable\": true } }],
+  \"default_lease_ttl\": \"${DEFAULT_LEASE_TTL}\",
+  \"max_lease_ttl\": \"${MAX_LEASE_TTL}\",
+  \"ui\": ${UI_ENABLED}
+}
+" > config.json
