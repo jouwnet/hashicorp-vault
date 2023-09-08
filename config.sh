41a4a5234452577bd08f4808bc1413a3d4fd9f7f
FournyP <pierre.fourny76@gmail.com>
2023-09-08 14:52:09 +0200

    Add disable_mlock config

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@ echo "
   \"listener\": [{ \"tcp\": { \"address\": \"0.0.0.0:8200\", \"tls_disable\": true } }],
   \"default_lease_ttl\": \"${DEFAULT_LEASE_TTL}\",
   \"max_lease_ttl\": \"${MAX_LEASE_TTL}\",
-  \"ui\": ${UI_ENABLED}
+  \"ui\": ${UI_ENABLED},
+  \"disable_mlock\": true
 }
 " > config.json
