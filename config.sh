36a8d9a86bf7e0274594b6116eda85102549308c
nick kuijpers <114398724+jouwnet@users.noreply.github.com>
2025-08-31 17:26:05 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,12 @@
 echo "
 {
   \"storage\": { \"file\": { \"path\": \"${STORAGE_PATH}\" } },
-  \"listener\": [{ \"tcp\": { \"address\": \"[::]:8200\", \"tls_disable\": \"true\" } }],
+  \"listener\": [{ \"tcp\": { \"address\": \"0.0.0.0:8200\", \"tls_disable\": \"true\" } }],
+  \"api_addr\": \"http://0.0.0.0:8200\",
   \"default_lease_ttl\": \"${DEFAULT_LEASE_TTL}\",
   \"max_lease_ttl\": \"${MAX_LEASE_TTL}\",
   \"ui\": ${UI_ENABLED},
   \"disable_mlock\": true
 }
 " > config.json
+d
